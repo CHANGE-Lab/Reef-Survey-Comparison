a0b14d43d776248d7b2fed89c00b16f4043bfe52
Iris George <69059695+iris-george@users.noreply.github.com>
2024-05-06 13:52:25 -0400

    Removed unused files

--- src/RVC_area_range.R
+++ src/RVC_area_range.R
@@ -1,8 +1,20 @@
-### Code to pull RVC data frame from GitHub ###
+########## SURVEY COMPARISON PROJECT RVC AREA RANGE ##########
+########## 
+##########
+# This file extracts RVC (i.e. SVC) survey data from a publicly available 
+# dataset and determines the range in total survey area to compare to the 
+# range in area observed in our data. 
+##########
+##########
+# AUTHOR: Iris M. George
+# DATE OF CREATION: 2024-05-06
+##########
+##########
 
 
 # rvc -> install and load the South Florida Reef Visual Census package from
 # https://github.com/jeremiaheb/rvc
+
 install.packages('devtools')
 library(devtools)
 devtools::install_github('jeremiaheb/rvc')
@@ -17,5 +29,3 @@ rvc_occurence <- getSampleData(years = c(2014, 2015), regions = "FLA KEYS")
 # calculate survey radius 
 rvc_occurence$radius <- ifelse(rvc_occurence$UNDERWATER_VISIBILITY < 7.5, 
                                rvc_occurence$UNDERWATER_VISIBILITY, 7.5)
-  
-# 
